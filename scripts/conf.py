018c9a32b10511b7a3bb0cb4f981bbf7bc03231f
Kim Scott <kimscott@mit.edu>
2016-09-26 15:40:25 -0400

    Set conf.py to use .env-prod as default and set VERSION according to env filename for use in selecting account data file, etc.

--- scripts/conf.py
+++ scripts/conf.py
@@ -6,8 +6,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', type=str)
 args, _ = parser.parse_known_args()
 
-dotenv_path = os.path.join(os.path.dirname(__file__), args.config or '.env')
+default_env = '.env-prod'
+dotenv_path = os.path.join(os.path.dirname(__file__), args.config or default_env)
 load_dotenv(dotenv_path)
+
+VERSION=(args.config or default_env).split('-')[-1]
 
 OSF_ACCESS_TOKEN = os.environ.get('OSF_ACCESS_TOKEN')
 JAM_NAMESPACE = os.environ.get('JAMDB_NAMESPACE')
