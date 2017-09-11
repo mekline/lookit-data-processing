5689d15b1ea7a90540dda830eb592887364ab4fd
Kim Scott <kimscott@mit.edu>
2017-09-11 18:00:38 -0400

    make separate timestamped logs when running automatically

--- scripts/scheduling/updateLookit.sh
+++ scripts/scheduling/updateLookit.sh
@@ -1,6 +1,10 @@
 source fwenv/bin/activate
 say "Updating Lookit studies and sending reminder emails"
 
+now=$(date +"%m_%d_%Y")
+timestamp=$(date +"%Y_%m_%d_%H_%M_%S")
+mv /Users/kms/lookit/scripts/logs/autoSync.out /Users/kms/lookit/scripts/logs/autoSync_$timestamp.out
+
 frameworkpython coding.py update --study physics
 frameworkpython reminder_emails.py --study physics --emails all
 frameworkpython coding.py fetchconsentsheet --coder Kim --study physics
@@ -11,8 +15,6 @@ frameworkpython coding.py fetchconsentsheet --coder Kim --study geometry
 frameworkpython coding.py updateaccounts --study geometry
 frameworkpython announce_geometry.py --emails all
 
-now=$(date +"%m_%d_%Y")
-
 aws s3 cp /Users/kms/lookitcoding/coding/58cc039ec0d9d70097f26220_Kim.csv s3://lookitgeometry/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
 
 aws s3 cp /Users/kms/lookitcoding/coding/accountsprod_58cc039ec0d9d70097f26220.csv s3://lookitgeometry/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
@@ -22,4 +24,5 @@ aws s3 cp /Users/kms/lookitcoding/coding/58cc039ec0d9d70097f26220_Kim.csv s3://l
 aws s3 cp /Users/kms/lookitcoding/coding/accountsprod_58cc039ec0d9d70097f26220.csv s3://lookitgeometry/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
 
 aws s3 sync /Users/kms/lookitcoding/sessions/58cc039ec0d9d70097f26220/ s3://lookitgeometry/processed/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
+
 
