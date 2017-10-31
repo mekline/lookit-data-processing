a65a7f6d8c124afc5e37195a7c59e9639b3b260e
Kim Scott <kimscott@mit.edu>
2017-10-31 13:09:58 -0400

    only track scripts

--- app/helpers/include.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function include(params) {
-    const [items, value] = params;
-    return items && items.indexOf(value) > -1;
-}
-
-export default Ember.Helper.helper(include);
