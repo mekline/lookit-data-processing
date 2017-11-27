93ef49fb8e6610f05dc7e71c03d8008a0f9bcf1c
Kim Scott <kimscott@mit.edu>
2017-11-27 09:34:18 -0500

    update automation for v2

--- scripts/scheduling/updateLookit.sh
+++ scripts/scheduling/updateLookit.sh
@@ -1,28 +1,27 @@
 source fwenv/bin/activate
-say "Updating Lookit studies and sending reminder emails"
+say "Updating Lookit version 2 studies and sending reminder emails"
 
 now=$(date +"%m_%d_%Y")
 timestamp=$(date +"%Y_%m_%d_%H_%M_%S")
 mv /Users/kms/lookit/scripts/logs/autoSync.out /Users/kms/lookit/scripts/logs/autoSync_$timestamp.out
 
+frameworkpython coding.py updateaccounts
 frameworkpython coding.py update --study physics
 frameworkpython reminder_emails.py --study physics --emails all
 frameworkpython coding.py fetchconsentsheet --coder Kim --study physics
-frameworkpython coding.py updateaccounts --study physics
+frameworkpython coding.py exportaccounts --study physics
 
 frameworkpython coding.py update --study geometry
 frameworkpython coding.py fetchconsentsheet --coder Kim --study geometry
-frameworkpython coding.py updateaccounts --study geometry
+frameworkpython coding.py exportaccounts --study geometry
 frameworkpython announce_geometry.py --emails all
 
-aws s3 cp /Users/kms/lookitcoding/coding/58cc039ec0d9d70097f26220_Kim.csv s3://lookitgeometry/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
+aws s3 cp /Users/kms/lookitcodingmultilab/coding/c7001e3a-cfc5-4054-a8e0-0f5e520950ab_Kim.csv s3://lookitgeometry/v2/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
 
-aws s3 cp /Users/kms/lookitcoding/coding/accountsprod_58cc039ec0d9d70097f26220.csv s3://lookitgeometry/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
+aws s3 cp /Users/kms/lookitcodingmultilab/coding/accountsprod_c7001e3a-cfc5-4054-a8e0-0f5e520950ab.csv s3://lookitgeometry/v2/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
 
-aws s3 cp /Users/kms/lookitcoding/coding/58cc039ec0d9d70097f26220_Kim.csv s3://lookitgeometry/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
+aws s3 cp /Users/kms/lookitcodingmultilab/coding/c7001e3a-cfc5-4054-a8e0-0f5e520950ab_Kim.csv s3://lookitgeometry/v2/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
 
-aws s3 cp /Users/kms/lookitcoding/coding/accountsprod_58cc039ec0d9d70097f26220.csv s3://lookitgeometry/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
+aws s3 cp /Users/kms/lookitcodingmultilab/coding/accountsprod_c7001e3a-cfc5-4054-a8e0-0f5e520950ab.csv s3://lookitgeometry/v2/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
 
-aws s3 sync /Users/kms/lookitcoding/sessions/58cc039ec0d9d70097f26220/ s3://lookitgeometry/processed/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
-
-
+aws s3 sync /Users/kms/lookitcodingmultilab/sessions/c7001e3a-cfc5-4054-a8e0-0f5e520950ab/ s3://lookitgeometry/v2/processed/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
