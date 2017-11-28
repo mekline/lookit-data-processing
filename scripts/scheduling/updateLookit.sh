0b71dcc5bfbfcdc1f370e3efc1e7f617414cb818
Kim Scott <kimscott@mit.edu>
2017-11-28 10:26:14 -0500

    stop doing updates for geometry study on v1

--- scripts/scheduling/updateLookit.sh
+++ scripts/scheduling/updateLookit.sh
@@ -10,23 +10,4 @@ frameworkpython reminder_emails.py --study physics --emails all
 frameworkpython coding.py fetchconsentsheet --coder Kim --study physics
 frameworkpython coding.py updateaccounts --study physics
 
-frameworkpython coding.py update --study geometry
-frameworkpython coding.py fetchconsentsheet --coder Kim --study geometry
-frameworkpython coding.py updateaccounts --study geometry
-frameworkpython announce_geometry.py --emails all
-
-frameworkpython experimenter.py download session58cc039ec0d9d70097f26220s --out ~/lookitcoding/coding/geometry.json
-
-aws s3 cp /Users/kms/lookitcoding/coding/58cc039ec0d9d70097f26220_Kim.csv s3://lookitgeometry/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
-
-aws s3 cp /Users/kms/lookitcoding/coding/accountsprod_58cc039ec0d9d70097f26220.csv s3://lookitgeometry/csv/$now/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
-
-aws s3 cp /Users/kms/lookitcoding/coding/58cc039ec0d9d70097f26220_Kim.csv s3://lookitgeometry/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
-
-aws s3 cp /Users/kms/lookitcoding/coding/geometry.json s3://lookitgeometry/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
-
-aws s3 cp /Users/kms/lookitcoding/coding/accountsprod_58cc039ec0d9d70097f26220.csv s3://lookitgeometry/csv/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
-
-aws s3 sync /Users/kms/lookitcoding/sessions/58cc039ec0d9d70097f26220/ s3://lookitgeometry/processed/ --grants read=emailaddress=babylab2@g.harvard.edu full=emailaddress=lookit@mit.edu
-
 
